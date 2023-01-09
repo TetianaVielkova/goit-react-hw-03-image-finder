6ce488fb49b2e4a9f711ac9dcdcf6c9b1b2aaa50
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2023-01-09 17:50:26 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import { Component } from "react";
 
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {getImageApi} from './servises/api';
+import {STATUS} from './constans/status';
+
 import { ToastContainer } from 'react-toastify';
 
+import { Button } from "components/Button/Button";
 import { Searchbar } from "./Searchbar/Searchbar";
 import {ImageGallery} from './ImageGallery/ImageGallery';
 
@@ -10,17 +17,49 @@ export class App extends Component{
 
   state={
     imageName: '',
-    
+    page: 1,
+    images: [],
+    status: STATUS.idle,
   }
 
+  async componentDidUpdate(_, prevState){
+    const prevName = prevState.imageName;
+    const newName = this.state.imageName;
+
+    const {page} = this.state;
+
+    if (prevName !== newName) {
+        this.setState({ page: 1, images: [] })
+    }
+
+    if (prevName !== newName || prevState.page !== page) {
+        this.setState({ status: STATUS.pending });
+    try{
+        const {data} = await getImageApi(newName, page);
+        if(data.total === 0){
+            this.setState({ status: STATUS.rejected });
+            return toast.warning(' Name not found. Try again ');
+        }
+        data.hits.map(image => {
+        return this.setState(({images}) => ({images: [...images, image]}));
+    })
+    }catch(error){
+        console.log(error);
+    }
+    this.setState({ status: STATUS.success });
+}
+}
 
   handleSearch = imageName => {
-    this.setState({ imageName});
+    this.setState({ imageName, page: 1, images:[]});
   };
 
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+}
 
   render(){
-    const {imageName} = this.state;
+    const {images, status} = this.state;
     return (
       <>
         <Searchbar onSubmit={this.handleSearch}/>
@@ -36,7 +75,8 @@ export class App extends Component{
           pauseOnHover
           theme="colored"
           />
-        <ImageGallery imageName={imageName}/>
+        <ImageGallery images={images}/>
+        {images.length >= 12  && status === STATUS.success && <Button handleLoadMore={this.handleLoadMore} />}
       </>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,59 +1,19 @@
-import PropTypes  from "prop-types";
 import { Component } from "react";
 
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-import {getImageApi} from './../servises/api';
 import {STATUS} from './../constans/status';
 import {List} from './ImageGallery.styled';
 import {Loader} from './../Loader/Loader';
 import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem";
-import { Button } from "components/Button/Button";
 
 export class ImageGallery extends Component  {
 
 state = {
-    images: [],
-    page: 1,
-    loading: false,
     status: STATUS.idle,
 }
 
-async componentDidUpdate(prevProps, prevState){
-    const prevName = prevProps.imageName;
-    const newName = this.props.imageName;
-
-    const {page} = this.state;
-
-    if (prevName !== newName) {
-        this.setState({ images: [] })
-    }
-
-    if (prevName !== newName || prevState.page !== page) {
-        this.setState({ status: STATUS.pending });
-    try{
-        const {data} = await getImageApi(newName, page);
-        if(data.total === 0){
-            this.setState({ status: STATUS.rejected });
-            return toast.warning(' Name not found. Try again ');
-        }
-        data.hits.map(image => {
-        return this.setState(({images}) => ({images: [...images, image]}));
-    })
-    }catch(error){
-        console.log(error);
-    }
-    this.setState({ status: STATUS.success });
-}
-}
-
-handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-}
-
 render(){
-    const { status, images } = this.state;
+    const { status } = this.state;
+    const {images} = this.props;
     return (
         <>
             { status === STATUS.pending  && <Loader/>}
@@ -62,12 +22,9 @@ render(){
                     <ImageGalleryItem  key={id} tags={tags} webformatURL={webformatURL} largeImageURL={largeImageURL} />
                 ))}
             </List>
-            {images.length >= 12  && status === STATUS.success && <Button handleLoadMore={this.handleLoadMore} />}
         </>
         );
     }
 }
 
-ImageGallery.propTypes = {
-    imageName: PropTypes.string.isRequired,
-}
+
